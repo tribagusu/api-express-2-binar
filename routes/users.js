f69e23e716f0a2e17698381dcfb5f1610842a722
tribagus <tri.bagus21@gmail.com>
2022-09-27 21:32:44 +0700

    add put method to update data

--- routes/users.js
+++ routes/users.js
@@ -2,41 +2,43 @@ const express = require("express")
 
 const router = express()
 
-const dataUser = [
-  {
-    id: 1,
-    name: "shol",
-    role: "user",
-  },
-  {
-    id: 2,
-    name: "jum",
-    role: "user",
-  },
-  {
-    id: 3,
-    name: "yay",
-    role: "user",
-  },
-]
+let dataUser = []
 
-// get data
+//_ get data
 router.get("/", (req, res) => {
   res.send(dataUser)
 })
 
-// post data
+//_ post data
 router.post("/", (req, res) => {
   const payload = req.body
   dataUser.push(payload)
   res.send("data berhasil ditambahkan")
 })
 
-// get single data
+//_ get single data
 router.get("/:id", (req, res) => {
-  const userId = req.params
-  const user = dataUser.find((item) => (item.id = userId.id))
-  res.send(user)
+  const param = req.params
+  const data = dataUser.find((item) => item.id == param.id)
+  res.send(data)
+})
+
+//_ delete data
+router.delete("/:id", (req, res) => {
+  const param = req.params
+  const data = dataUser.filter((item) => item.id != param.id)
+  res.send(data)
+})
+
+//_ update data
+router.put("/:id", (req, res) => {
+  const param = req.params
+  const newName = req.body.name
+  const newUsers = dataUser.map((item) => {
+    return item.id == param.id ? { ...item, name: newName } : item
+  })
+  dataUser = newUsers
+  res.send(newUsers)
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express")
 const userRouter = require("./routes/users")
 
-// panggil express
+//# panggil express
 const app = express()
-// panggil express json
+
+//# panggil express json
 app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("halo hai")
 })
 
+// import router user
 app.use("/users", userRouter)
 
-const PORT = 8000
-
 // listen untuk mendengarkan jalan di port berapa
+const PORT = 8000
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`)
 })
