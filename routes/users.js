addcb04b8c613db3a696082ff405cc3080b45ca1
tribagus <tri.bagus21@gmail.com>
2022-09-27 20:31:19 +0700

    get single user with params

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,39 @@ const router = express()
 
 const dataUser = [
   {
-    id: "2",
+    id: 1,
     name: "shol",
+    role: "user",
+  },
+  {
+    id: 2,
+    name: "jum",
+    role: "user",
+  },
+  {
+    id: 3,
+    name: "yay",
     role: "user",
   },
 ]
 
+// get data
 router.get("/", (req, res) => {
   res.send(dataUser)
 })
 
+// post data
 router.post("/", (req, res) => {
   const payload = req.body
   dataUser.push(payload)
   res.send("data berhasil ditambahkan")
+})
+
+// get single data
+router.get("/:id", (req, res) => {
+  const userId = req.params
+  const user = dataUser.find((item) => (item.id = userId.id))
+  res.send(user)
 })
 
 module.exports = router
