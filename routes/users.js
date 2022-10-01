6a98b24ac659dd0fd146c7332bab34f90aedba79
tribagus <tri.bagus21@gmail.com>
2022-10-01 10:06:29 +0700

    add post method api

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const { db } = require("../config/dbConfig")
 
 router.get("/", (req, res) => {
   const sqlQuery = "SELECT * FROM user"
-
   db.query(sqlQuery, (error, result) => {
     if (error) {
       console.log(error)
@@ -25,6 +24,23 @@ router.get("/:id", (req, res) => {
       res.send(result)
     }
   })
+})
+
+router.post("/", (req, res) => {
+  const payload = req.body
+  const sqlQuery = `INSERT INTO user(user_name, user_email, user_password) VALUES (?,?,?)`
+
+  db.query(
+    sqlQuery,
+    [payload.user_name, payload.user_email, payload.user_password],
+    (error, result) => {
+      if (error) {
+        console.log(error)
+      } else {
+        res.send(result)
+      }
+    }
+  )
 })
 
 module.exports = router
